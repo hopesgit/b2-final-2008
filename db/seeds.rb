f7d4a7f8a0eb14d51680faa45ac18646e506b7d0
Hope Gochnour <hope.gochnour@gmail.com>
2020-11-09 09:30:37 -0700

    Seeds and tables created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Doctor.create(Name: "Meredith Grey", Specialty: "General Surgery", Education: "Harvard University")
+Doctor.create(Name: "Alex Karev", Specialty: "Pediatric Surgery", Education: "Johns Hopkins University")
+Doctor.create(Name: "Miranda Bailey", Specialty: "General Surgery", Education: "Stanford University")
+Doctor.create(Name: "Derek McDreamy Shepherd", Specialty: "Attending Surgeon", Education: "University of Pennsylvania")
+
+Hospital.create(Name: "Grey Sloan Memorial Hospital")
+Hospital.create(Name: "Seaside Health & Wellness Center")
+
+Patient.create(Name: "Katie Bryce", Age: 24)
+Patient.create(Name: "Denny Duquette", Age: 39)
+Patient.create(Name: "Rebecca Pope", Age: 32)
+Patient.create(Name: "Zola Shepherd", Age: 2)
