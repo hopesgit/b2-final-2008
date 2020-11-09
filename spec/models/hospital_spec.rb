847b3c1c2b7b4f693841be118d317212ab2a3714
Hope Gochnour <hope.gochnour@gmail.com>
2020-11-09 10:42:46 -0700

    Add test for Hospitals#count-of_doctors

--- spec/models/hospital_spec.rb
+++ spec/models/hospital_spec.rb
@@ -8,4 +8,16 @@ describe Hospital, type: :model do
   describe "relationships" do
     it { should have_many :doctors }
   end
+
+  describe "instance methods" do
+    before :each do
+      @memorial = Hospital.create(name: "Grey Sloan Memorial Hospital")
+      @meredith = Doctor.create(name: "Meredith Grey", specialty: "General Surgery", education: "Harvard University", hospital_id: @memorial.id)
+      @alex = Doctor.create(name: "Alex Karev", specialty: "Pediatric Surgery", education: "Harvard University", hospital_id: @memorial.id)
+    end
+    
+    it "#count_of_doctors" do
+      expect(@memorial.count_of_doctors).to eq(2)
+    end
+  end
 end
